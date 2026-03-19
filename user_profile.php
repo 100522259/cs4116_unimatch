1a99b943dc654b0ebfcb68e504093f0e0dc3b28d
100522259 <100522259@alumnos.uc3m.es>
2026-03-19 10:27:09 +0000

    user_profile page

---
+++ user_profile.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>unimatch - user profile page</title>
+        <!--Bootstrap css-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+            rel="stylesheet">
+        <link href=".\css\syle.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <!--vertical container for page index-->
+            <div class="col-2">
+
+            </div>
+            <!--Container for the rest of the page-->
+            <div class="col-10">
+                <div class="main_info">
+                    <div class="pfp">
+
+                    </div>
+                    <div class="info">
+
+                    </div>
+                </div>
+                <div class="my_images_txt">
+
+                </div>
+                <div class="my_images">
+                <!--temp code; using php we'd request the number of photos and create as many
+                    divs as photos available-->
+                    <div class="photo">
+
+                    </div>
+                    <div class="photo">
+                        
+                    </div>
+                    <div class="photo">
+                        
+                    </div>
+                    <div class="photo">
+                        
+                    </div>
+                    <div class="photo">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
