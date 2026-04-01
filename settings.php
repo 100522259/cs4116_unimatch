fe0d83f113e57073fef046f17c755e20fc7c555c
100522259 <100522259@alumnos.uc3m.es>
2026-04-01 17:59:43 +0100

    settings page

---
+++ settings.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>unimatch - admin page</title>
+
+        <!--Bootstrap css--
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+            rel="stylesheet">
+        <link rel="stylesheet" 
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.8/css/bootstrap-grid.min.css" 
+            integrity="sha512-dOjUSaLkr6G2pwQ7ry9juX+iXw5602zg1kg8yH+guR3uSEidGyCnOEQnGlr7xwu/8WE+pVm1ZNqaIs5ETTIJQg==" 
+            crossorigin="anonymous" referrerpolicy="no-referrer"/>-->
+
+        <link href="css\profile.css" rel="stylesheet">
+        <link href="css\profile_mobile.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <!--vertical container for page index-->
+            <div class="index">
+                <div class="logo">
+                    <img class="logo" src="images\logo5.png">
+                </div>
+                <div class="idx_icons">
+                    <?php
+                    $icons = array("home", "dates", "friends","chat",
+                                    "user","settings","admin");
+                    foreach ($icons as $icon) {
+                        echo "<a href=\"{$icon}.php\">";    
+                        echo "<div class=\"pages\">";
+                        echo "<div class=\"icons\"><img src=\"images\\{$icon}.png\"></div>";
+                        echo "<div class=\"icons\"><p>{$icon}</p></div>";
+                        echo "</div></a>";
+                    }
+                    ?>
+                
+                </div>
+            </div>
+            <!--Container for the rest of the page-->
+            <div class="main">
+
+                <form name="credentials_settings" action="" method="on" autocomplete="off">
+                    <fieldset><legend>Credentials</legend><br>
+                    
+                        <label for="username">Username: </label>
+                        <input type="text" name="username" 
+                            pattern="[A-Za-z0-9]{5,}" maxlength="20" size="20"
+                            title="Write your username, between 5 and 20 characters">
+                        <br><br>
+                        
+                        <label for="password">Password: </label>
+                        <input type="password" name="password" id="myPsw"
+                            pattern="^[^;]+$" minlength="8" maxlength="20" size="20"
+                            title="Write your username, between 8 and 20 characters">
+                        <br>
+                        <!-- An element to toggle between password visibility 
+                        Note: taken from w3schools-->
+                        <label for="visibility">Show Password </label>
+                        <input type="checkbox" name="visibility" onclick="toggleVisib()"><br><br>
+
+                        <!-- TODO Passwords must be equal -->
+                        <label for="password2">Confirm new Password: </label>
+                        <input type="password" name="password2" id="myPsw2"
+                            pattern="^[^;]+$" minlength="8" maxlength="20" size="20"
+                            title="Write your username, between 8 and 20 characters">
+                        <br>
+                        
+                        <label for="visibility">Show Password </label>
+                        <input type="checkbox" name="visibility" onclick="toggleVisib2()">
+                        <br><br>
+
+                    </fieldset>
+                    <input type="submit" name="submit_cred" value="Save Changes">
+                </form><br><br>
+
+                <!--Form for the user to change their user details-->
+                <form name="basic_settigns" action="" method="on" autocomplete="off">
+                    <fieldset><legend>Personal Info</legend><br>
+                    
+                        <label for="fname">First Name: </label>
+                        <input type="text" name="fname" 
+                            pattern="[A-Za-z]{2,}" maxlength="30" size="30"
+                            title="Write your first name, between 2 and 30 characters">
+                        <br><br>
+                        
+                        <label for="lname">Last name:</label>
+                        <input type="text" name="lname" 
+                        pattern="[A-Za-z]{2-30}" maxlength="30" size="30"
+                            title="Write your last name, between 2 and 30 characters">
+                        <br><br>
+
+                        <label for="age">Age:</label>
+                        <select id="age" name="Age">
+                            <option value="" disabled selected>Select age</option>
+                            <?php
+                            for ($i=17; $i<=30; $i++) {
+                                echo "<option value=\"{$i}\">{$i}</option>";
+                            }
+                            ?>
+                        </select>
+                        <br><br>
+
+                        <label for="county">County:</label>
+                        <input type="text" name="county" 
+                            pattern="[A-Za-z]{2-30}" maxlength="30" size="30"
+                            title="Write your county, between 2 and 30 characters">
+                        <br><br>
+
+                        <label for="nationality">Nationality:</label>
+                        <input type="text" name="nationality" 
+                            pattern="[A-Za-z]{2-30}" maxlength="30" size="30"
+                            title="Write your nationality, between 2 and 30 characters">
+                        <br><br>
+
+                        <label for="gender">Gender:</label>
+                        <select id="gender" name="Gender">
+                            <option value="" disabled selected>Select gender</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="non-binary">Non-binary</option>
+                            <option value="prefer-not-to-say">Prefer not to say</option>
+                        </select>
+                        <br><br>
+
+                        <label for="bio">Bio:</label>
+                        <!--Note: make sure that the user can't write anything with ";"
+                        To prevent sql injection.
+                        The regex was extracted from a ChatGPT prompt: it ensures that the string
+                        can empty (*) and reject the semi-colon. -->
+                        <input class="resize-box" type="text" name="bio" 
+                            pattern="^[^;]*$" maxlength="2500" size="100"
+                            title="Write your last name, between 2 and 30 characters">
+                        <br><br>
+                    </fieldset>
+                    <input type="submit" name="submit_pers" value="Save Changes">
+                </form><br><br>
+
+                <form>
+                    <fieldset><legend>Academic Info</legend><br>
+                    
+                        <label for="course">Course: </label>
+                        <input type="text" name="course" 
+                            pattern="[A-Za-z ]{2,}" maxlength="20" size="20"
+                            title="Write your course, between 2 and 20 characters">
+                        <br><br>
+                        
+                        <label for="year">Year: </label>
+                        <input type="text" name="year" 
+                            pattern="[1-9]{1}" maxlength="1" size="5"
+                            title="Write your course year, between 1 and 9">
+                        <br><br>
+                        
+                    </fieldset>
+                    <input type="submit" name="submit_acad" value="Save Changes">
+                </form><br><br>
+
+                <form>
+                    <fieldset><legend>Interests</legend><br>
+                    
+                        <label for="drink">Drinking habits: </label>
+                        <select name="drink">
+                            <option value="" disabled selected>Select habit</option>
+                            <option value="no">No</option>
+                            <option value="yes">Yes</option>
+                            <option value="socially">Socially</option>
+                            <option value="occasionally">Occasionally</option>
+                        </select>
+                        <br><br>
+
+                        <label for="smoke">Smoking habits: </label>
+                        <select name="smoke">
+                            <option value="" disabled selected>Select habit</option>
+                            <option value="no">No</option>
+                            <option value="yes">Yes</option>
+                            <option value="socially">Socially</option>
+                            <option value="occasionally">Occasionally</option>
+                        </select>
+                        <br><br>
+
+                        <label for="food">Food Lifestyle: </label>
+                        <select name="food">
+                            <option value="" disabled selected>Select lifestyle</option>
+                            <option value="normal">Normal</option>
+                            <option value="vegetarian">Vegetarian</option>
+                            <option value="vegan">Vegan</option>
+                            <option value="pescatarian">Pescatarian</option>
+                            <option value="other">Other</option>
+                        </select>
+                        <br>
+                        <label for="disp_lifestyle">Display: </label>
+                        <input type="checkbox" name="disp_lifestyle">
+                        <br><br>
+
+                        <label for="personality">Personality: </label>
+                        <select name="personality">
+                            <option value="" disabled selected>Select personality</option>
+                            <option value="extrovert">Extrovert</option>
+                            <option value="introvert">Introvert</option>
+                            <option value="ambivert">Ambivert</option>
+                        </select>
+                        <br>
+                        <label for="disp_personality">Display: </label>
+                        <input type="checkbox" name="disp_personality">
+                        <br><br>
+
+                        <label for="sexuality">Sexuality: </label>
+                        <select name="sexuality">
+                            <option value="" disabled selected>Select sexuality</option>
+                            <option value="straight">Straight</option>
+                            <option value="gay">Gay</option>
+                            <option value="lesbian">Lesbian</option>
+                            <option value="bisexual">Bisexual</option>
+                            <option value="pansexual">Pansexual</option>
+                            <option value="asexual">Asexual</option>
+                            <option value="other">Other</option>
+                        </select>
+                        <br>
+                        <label for="disp_sexuality">Display: </label>
+                        <input type="checkbox" name="disp_sexuality">
+                        <br><br>
+
+                        <?php
+                        // Section for interest, repeated five times
+                        $interest_ops = array("Sports", "Music", "Gaming", "Reading",
+                        "Travel", "Cooking", "Fitness", "Photography", "Art", "Technology",
+                        "Movies", "Fashion", "Nature", "Dance", "Writing");
+                        for ($i=1; $i<6; $i++) {
+                            echo "<label for=\"interest{$i}\">Interest {$i}: </label>";
+                            echo "<select name=\"interest{$i}\">";
+                            echo "<option value=\"none\" disabled selected>Select interest</option>";
+                            foreach ($interest_ops as $int) {
+                                echo "<option value=\"{$int}\">{$int}</option>";
+                            }
+                            echo "</select><br><br>";
+                        }
+                        ?>                      
+                        
+                    </fieldset>
+                    <input type="submit" name="submit_int" value="Save Changes">
+                </form><br><br>
+
+                <form>
+                    <fieldset><legend>Image Upload</legend><br>
+                    <label for="pfp">Upload Profile Photo: </label>
+                    <input type="file" name="pfp"><br><br>
+                    <?php
+                        for ($i=1; $i<6; $i++) {
+                            echo "<label for=\"image{$i}\">Upload Image {$i}: </label>";
+                            echo "<input type=\"file\" name=\"image{$i}\"><br><br>";
+                        }
+                    ?>
+                    </fieldset>
+                </form><br><br>
+            </div>
+        </div>
+        
+        
+        <script>
+            // Makes password visible on toggle -- taken from w3schools tutorial
+            function toggleVisib() {
+                var x = document.getElementById("myPsw");
+                if (x.type === "password") {
+                    x.type = "text";
+                } else {
+                    x.type = "password";
+                }
+            }
+
+            function toggleVisib2() {
+                var x = document.getElementById("myPsw2");
+                if (x.type === "password") {
+                    x.type = "text";
+                } else {
+                    x.type = "password";
+                }
+            }
+        </script>
+    </body>
+</html>
--- user_queries.php
+++ user_queries.php
@@ -1,38 +1,38 @@
 <?php
-    //echo "<h1>Trying to query</h1>";
-    $servername = "sql108.infinityfree.com";
-    $username = "if0_41207740";
-    $password = "hpQ2aHXoodn";
+//echo "<h1>Trying to query</h1>";
+$servername = "sql108.infinityfree.com";
+$username = "if0_41207740";
+$password = "hpQ2aHXoodn";
 
-    $dbname = "if0_41207740_unimatch_db";
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$dbname = "if0_41207740_unimatch_db";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: ".$conn->connect_error);
-    }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error);
+}
 
-    //echo "Connected successfully<br>";
-    $user_id = 200000004;
+//echo "Connected successfully<br>";
+$user_id = 200000004;
 
-    // 1. select image content
-    $sql = "select * from images where user_id={$user_id};";
-    $result = $conn->query($sql);
-    $images = $result->fetch_assoc();
-    //echo "{$user_id}: <img src=\"/unimatch/images/{$row["profile_pic"]}\">";
+// 1. select image content
+$sql = "select * from images where user_id={$user_id};";
+$result = $conn->query($sql);
+$images = $result->fetch_assoc();
+//echo "{$user_id}: <img src=\"/unimatch/images/{$row["profile_pic"]}\">";
 
-    // 2. select profile content:
-    $sql = "select * from personal_info where user_id={$user_id};";
-    $result = $conn->query($sql);
-    $pers_info = $result->fetch_assoc();
+// 2. select profile content:
+$sql = "select * from personal_info where user_id={$user_id};";
+$result = $conn->query($sql);
+$pers_info = $result->fetch_assoc();
 
-    // 3. Interests:
-    $sql = "select * from interests where user_id={$user_id};";
-    $result = $conn->query($sql);
-    $ints = $result->fetch_assoc();
+// 3. Interests:
+$sql = "select * from interests where user_id={$user_id};";
+$result = $conn->query($sql);
+$ints = $result->fetch_assoc();
 
-    // 4. about uni:
-    $sql = "select * from academic_info where user_id={$user_id};";
-    $result = $conn->query($sql);
-    $uni = $result->fetch_assoc();
+// 4. about uni:
+$sql = "select * from academic_info where user_id={$user_id};";
+$result = $conn->query($sql);
+$uni = $result->fetch_assoc();
 
