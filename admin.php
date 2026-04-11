a9884b4423a7b52beb3c15af02482a8030b4d51f
100522259 <100522259@alumnos.uc3m.es>
2026-04-11 11:48:06 +0100

    unified sidebar to single php file included in the rest

--- admin.php
+++ admin.php
@@ -19,35 +19,10 @@
 
     <body>
         <div class="container">
+            
             <!--vertical container for page index-->
-            <div class="index">
-                <div class="logo">
-                    <img class="logo" src="images\logo5.png">
-                </div>
-                <div class="idx_icons">
-                    <?php
-                    include "user_queries.php";
+            <?php include "./sidebar.php"; ?>
 
-                    $icons = array("home", "matches","messaging",
-                                    "user","settings");
-                    foreach ($icons as $icon) {
-                        echo "<a href=\"{$icon}.php\">";    
-                        echo "<div class=\"pages\">";
-                        echo "<div class=\"icons\"><img src=\"images\\{$icon}.png\"></div>";
-                        echo "<div class=\"icons\"><p>{$icon}</p></div>";
-                        echo "</div></a>";
-                    }
-                    if ($admin) {
-                        echo "<a href=\"admin.php\">";    
-                        echo "<div class=\"pages\">";
-                        echo "<div class=\"icons\"><img src=\"images\\admin.png\"></div>";
-                        echo "<div class=\"icons\"><p>admin</p></div>";
-                        echo "</div></a>";
-                    }
-                    ?>
-                
-                </div>
-            </div>
             <!--Container for the rest of the page-->
             <div class="main">
                 <div class="header-text">
