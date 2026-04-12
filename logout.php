b3c457b689a3b4242fd0f4829fc0d5e39cb01577
Mohamed Amine <23397403@studentmail.ul.ie>
2026-04-12 12:49:42 +0100

    modify logout.php to remove all session variables and destroy cookie and data, for a clean logout

--- logout.php
+++ logout.php
@@ -1,4 +1,13 @@
 <?php
 // logout
+// session startt
+session_start();
+
+// remove all session variables
+session_unset();
+
+// destroy the seesion cookie and data
+session_destroy();
+
 header("Location: ./html/login.html");
 exit;
